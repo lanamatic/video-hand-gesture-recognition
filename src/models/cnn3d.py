eb5a3ab99302003773afbe049ed26b220d4ac904
Lana <matic02lana@gmail.com>
2026-09-02 23:41:20 +0200

    Add Keggle trained models and change results accordingly

--- src/models/cnn3d.py
+++ src/models/cnn3d.py
@@ -8,19 +8,22 @@ import torch.nn as nn
 class Simple3DCNN(nn.Module):
     def __init__(self, num_classes=13, dropout=0.5):
         super().__init__()
+        
+        def block(in_c, out_c):
+            return nn.Sequential(
+                nn.Conv3d(in_c, out_c, kernel_size=3, padding=1),
+                nn.BatchNorm3d(out_c),
+                nn.ReLU(inplace=True),
+                nn.MaxPool3d(2, 2),
+            )
 
         self.features = nn.Sequential(
-            nn.Conv3d(3, 32, kernel_size=3, padding=1),
-            nn.BatchNorm3d(32), nn.ReLU(inplace=True), nn.MaxPool3d(2),
-
-            nn.Conv3d(32, 64, kernel_size=3, padding=1),
-            nn.BatchNorm3d(64), nn.ReLU(inplace=True), nn.MaxPool3d(2),
-
-            nn.Conv3d(64, 128, kernel_size=3, padding=1),
-            nn.BatchNorm3d(128), nn.ReLU(inplace=True), nn.MaxPool3d(2),
-
+            block(3, 32),
+            block(32, 64),
+            block(64, 128),
             nn.Conv3d(128, 256, kernel_size=3, padding=1),
-            nn.BatchNorm3d(256), nn.ReLU(inplace=True),
+            nn.BatchNorm3d(256),
+            nn.ReLU(inplace=True),
             nn.AdaptiveAvgPool3d(1),
         )
 
